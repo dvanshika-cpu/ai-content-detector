c78a124d6cc9c49ef62968157b448347a19cd547
dvanshika-cpu <d.vanshika0706@gmail.com>
2026-09-14 23:43:13 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -69,13 +69,14 @@ def get_db_connection():
     try:
 
         db_host = os.environ.get("DB_HOST")
+        print("DB_HOST VALUE:", db_host)
         db_port = os.environ.get("DB_PORT")
         db_user = os.environ.get("DB_USER")
         db_password = os.environ.get("DB_PASSWORD")
         db_name = os.environ.get("DB_NAME")
 
         print("========== DATABASE DEBUG ==========")
-        print("DB_HOST:", db_host)
+        
         print("DB_PORT:", db_port)
         print("DB_USER:", db_user)
         print("DB_PASSWORD SET:", bool(db_password))
