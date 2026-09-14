2e5c186cdaa2071bdf9b1deef90bc4e8e1c71d41
dvanshika-cpu <d.vanshika0706@gmail.com>
2026-09-14 23:22:15 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -73,7 +73,7 @@ def get_db_connection():
             port=os.environ.get("DB_PORT", 3306),
             user=os.environ.get("DB_USER",),
             password=os.environ.get("DB_PASSWORD"),
-            database=os.environ.get("DB_NAME")
+            database=os.environ.get("DB_NAME","ai_analyse")
         )
 
         return db
