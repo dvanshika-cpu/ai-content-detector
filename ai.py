c450404671048a5468fa8c94ddd19a2c07ef7af5
dvanshika-cpu <d.vanshika0706@gmail.com>
2026-09-14 23:06:25 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -70,7 +70,7 @@ def get_db_connection():
 
         db = mysql.connector.connect(
             host=os.environ.get("DB_HOST", "localhost"),
-            port=os.eviron.get("DB_PORT", 3306),
+            port=os.environ.get("DB_PORT", 3306),
             user=os.environ.get("DB_USER",),
             password=os.environ.get("DB_PASSWORD"),
             database=os.environ.get("DB_NAME")
